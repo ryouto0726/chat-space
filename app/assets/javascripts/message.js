8e0e0b74f33399bc6a1b356fdad8d7a2fa16bf0a
ryouto0726 <0wz18r1w726076w@ezweb.ne.jp>
2019-09-11 01:49:29 +0900

    stashed change

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,67 +1,70 @@
 $(function(){
-  function buildHTML(message){
-    if ( message.image ) {
-      var html =
-       `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.date}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-          <img src=${message.image} >
-        </div>`
-      return html;
-    } 
-    else {
-      var html =
-       `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.date}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-        </div>`
-      return html;
-    };
+  function appendMessage(message) {
+    var html = `<div class="message" data-message-id=${message.id}>
+                  <div class='message__upper-info'>
+                  <div class="upper-message__user-name">${message.user_name}
+                  <div class="upper-message__date">${message.date}
+                  </div>
+                  </div>
+                  <div class="lower-message">
+                  <p class="lower-message__content">${message.content}</p>
+                  </div>
+                    ${ message.image !== null ? `<img class='lower-message__image' src='${message.image}'>` : `` }
+                  </div>`
+    $('.messages').append(html)
   }
-$('.new_message').on('submit', function(e){
-  e.preventDefault();
+  $('.new_message').on('submit', function(e){
+    e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
-     $.ajax({
-     url: url,
-     type: "POST",
-     data: formData,
-     dataType: 'json',
-     processData: false,
-    contentType: false
-     })
-     .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html); 
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      $('form')[0].reset();
- })
- 
- .fail(function(){
-  alert('error');
-});
-return false;
-})
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+    .done(function(message) {
+      appendMessage(message);
+      $('#new_message')[0].reset();
+      $('.submit-btn').prop('disabled', false);
+      $('.messages').animate({scrollTop: 999999}, 500, 'swing');
+    })
+    .fail(function() {
+      alert('メッセージを投稿できませんでした。');
+      $('.submit-btn').prop('disabled', false);
+    })
+  });
+
+  var reloadMessages = function() {
+    last_message_id = $('.message:last').data('message-id')
+    var urlRegex = new RegExp("groups/\[0-9]{1,}/messages")
+    var currentUrl = location.pathname
+
+    if( urlRegex.test(currentUrl) ) {
+      $.ajax({
+        type: 'get',
+        url: url,
+        dataType: 'json',
+        data: { id: last_message_id }
+      })
+
+      .done(function(messages) {
+        if(messages.length !== 0) {
+          messages.forEach(function (message) {
+            appendMessage(message);
+          });
+          $('.messages').animate({scrollTop: 999999}, 500, 'swing');
+        }
+        $('.submit-btn').prop('disabled', false);
+      })
+      .fail(function() {
+        $('.submit-btn').prop('disabled', false);
+      });
+    }
+  };
+
+  setInterval(reloadMessages, 5000);
 });
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
   def index
   end
 
@@ -19,11 +21,9 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
-   def update
-      @group = Group.find(params[:id])
+  def update
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
