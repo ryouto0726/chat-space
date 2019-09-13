8e073dc789d8df088759dd7dbb6f566dd4d382e7
ryouto0726 <0wz18r1w726076w@ezweb.ne.jp>
2019-09-13 19:35:17 +0900

    lncrement-search

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,9 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: [:edit, :update]
+  before_action :set_params, only: [:edit, :update]
 
   def index
+    @groups = Group.all
   end
-
- 
 
   def new
     @group = Group.new
@@ -14,30 +13,32 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to root_path, notice: 'グループを作成しました'
+      redirect_to root_path, notice: 'グループを作成しました。'
     else
       render :new
     end
   end
 
   def edit
-
   end
-
+  
   def update
+    #  byebug
     if @group.update(group_params)
-      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました。'
     else
       render :edit
     end
   end
 
   private
+
   def group_params
     params.require(:group).permit(:name, { user_ids: [] })
   end
 
-  def set_group
+  def set_params
     @group = Group.find(params[:id])
   end
+  
 end
