a670ea0eb7bd812334ea2ab178b0830edf85b2f8
ryouto0726 <0wz18r1w726076w@ezweb.ne.jp>
2019-09-18 17:49:26 +0900

    automatic-updating

---
+++ app/controllers/api/messages_controller.rb
@@ -0,0 +1,6 @@
+class Api::MessagesController < ApplicationController
+  def index
+    @group = Group.find(params[:group_id]) #今いるグループの情報をパラムスの値を元にDBから取得。
+    @messages = @group.messages.includes(:user).where('id > ?', params[:id])
+  end
+end
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -3,4 +3,3 @@ json.user_name @message.user.name
 json.date @message.created_at.strftime("%Y/%m/%d/ %H:%M/")
 json.content @message.content
 json.image @message.image_url
-
