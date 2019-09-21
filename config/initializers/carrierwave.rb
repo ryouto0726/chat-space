04d79228147b796b3a596c6773ecb1afceba6702
ryouto0726 <0wz18r1w726076w@ezweb.ne.jp>
2019-09-21 20:16:04 +0900

    update

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,16 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+# require 'carrierwave/storage/abstract'
+# require 'carrierwave/storage/file'
+# require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
+# CarrierWave.configure do |config|
+#   config.storage = :fog
+#   config.fog_provider = 'fog/aws'
+#   config.fog_credentials = {
+#     provider: 'AWS',
    
-    # region: 'ap-northeast-1' 
-  }
+#     # region: 'ap-northeast-1' 
+#   }
 
-  config.fog_directory  = 'chat-space-0726'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space-0726'
-end
+#   config.fog_directory  = 'chat-space-0726'
+#   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space-0726'
+# end
