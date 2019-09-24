c822c3d968a0fc1390e45c81d2c069fd4e605937
ryouto0726 <0wz18r1w726076w@ezweb.ne.jp>
2019-09-24 12:36:54 +0900

    update

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'chat-space-0726'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ckat-space'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space-0726'
 end
